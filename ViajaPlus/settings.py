a079474de7bbd4c48a9ae22f0920855e8a59409a
wlko <cristiangconci@gmail.com>
2023-11-30 18:22:59 -0300

    Config for staticfiles, change font-family

--- ViajaPlus/settings.py
+++ ViajaPlus/settings.py
@@ -15,6 +15,9 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [STATIC_DIR,]
 
 
 # Quick-start development settings - unsuitable for production
