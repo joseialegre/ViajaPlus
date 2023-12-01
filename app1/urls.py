9ccd489ae5ec7392fb6109def729c0fe83048e97
José Ignacio Alegre <joseignacioalegre4@gmail.com>
2023-12-01 08:26:34 -0300

    it fucking works

--- app1/urls.py
+++ app1/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     
     path('obtener_paradas_intermedias/<int:itinerario_id>/', obtener_paradas_intermedias, name='obtener_paradas_intermedias'),
     path('obtener_servicios_itinerarios/<int:itinerario_id>/', obtener_servicios_itinerarios, name='obtener_servicios_itinerarios'),
-    path('reservar_pasaje/', reservar_pasaje, name='reservar_pasaje'),
+    path('reservar_pasaje', reservar_pasaje, name='reservar_pasaje'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -53,18 +53,21 @@ def obtener_servicios_itinerarios(request, itinerario_id):
 
 
 def reservar_pasaje(request):
-    if request.method == 'POST':
-        dni_reserva = request.POST.get('dniReserva')
-        servicio_id = request.POST.get('itinerarios')  # Suponiendo que el valor es el ID del servicio
-        itinerario_codigo = Servicio.objects.get(pk=servicio_id).itinerario
 
-        # Crear el pasaje
-        pasaje = Pasaje.objects.create(
-            costo=100,  # Ajusta el costo según tus requisitos
-            servicio_id=servicio_id,
-            DNI=dni_reserva,
-            estado=0  # Ajusta el estado según tus necesidades
-        )
+    try:
+        if request.method == 'POST':
+            dni_reserva = request.POST.get('dniReserva')
+            servicio_id = request.POST.get('itinerarios')  # Suponiendo que el valor es el ID del servicio
+            itinerario_codigo = Servicio.objects.get(pk=servicio_id).itinerario
 
+            # Crear el pasaje
+            pasaje = Pasaje.objects.create(
+                costo=100,  # Ajusta el costo según tus requisitos
+                servicio_id=servicio_id,
+                DNI=dni_reserva,
+                estado=0  # Ajusta el estado según tus necesidades
+            )
+    except Exception as e:
+        print(e)
         # Realizar otras acciones o redireccionar según sea necesario
-        return JsonResponse({'mensaje': 'Pasaje reservado exitosamente'})
+    return JsonResponse({'mensaje': 'Pasaje reservado exitosamente'})
