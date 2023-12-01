a57adae0bb53761524bf44de796b2ebcbe538eeb
Alejo <alejoczombos@gmail.com>
2023-12-01 02:18:22 -0300

    Selección de Paradas y Servicios

--- app1/admin.py
+++ app1/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 
 from .models import *
 
+@admin.register(Itinerario)
+class ItinerarioAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre')
+    ordering = ('codigo',)
+    search_fields = ('codigo', 'nombre')
 
 admin.site.register(Ciudad)
-admin.site.register(Itinerario)
 admin.site.register(Parada)
 admin.site.register(Pasaje)
 admin.site.register(Servicio)
