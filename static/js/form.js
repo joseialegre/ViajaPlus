4d369d30c941a77e4d7adf5b1ee3c7f8e3cad84d
wlko <cristiangconci@gmail.com>
2023-12-02 21:57:36 -0300

    Front bastante estilizado, falta programar vista de servicios y guardar la reserva

--- static/js/form.js
+++ static/js/form.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Tu código JavaScript aquí
+    var origenes = document.getElementById('origen-container');
+    var destinos = document.getElementById('destino-container');
+    var form = document.getElementById('formReservas'); // Asegúrate de reemplazar 'formReservas' con el ID de tu formulario
+
     document.getElementById('itinerarios').addEventListener('change', function () {
         var itinerarioId = this.value;
 
@@ -8,47 +12,94 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(data => {
                 actualizarOpciones('origen', data.paradas_intermedias);
                 actualizarOpciones('destino', data.paradas_intermedias);
-            })
-            .catch(error => console.error('Error:', error));
-    });
-    document.getElementById('reservar-btn').addEventListener('click', function () {
-        var itinerarioId = document.getElementById('itinerarios').value;
 
-        fetch('/obtener_servicios_itinerarios/' + itinerarioId + '/')
-            .then(response => response.json())
-            .then(data => {
-                data.servicios.forEach(function (servicio) {
-                    alert('Servicio: ' + servicio.numero_servicio + ', Partida: ' + servicio.fecha_partida + ', Llegada: ' + servicio.fecha_llegada);
-                })
+                // Deshabilitar todos los destinos al inicio
+                deshabilitarDestinos();
             })
             .catch(error => console.error('Error:', error));
     });
+
+    function deshabilitarDestinos() {
+        var radiosDestino = destinos.querySelectorAll('input[type="radio"]');
+        radiosDestino.forEach(function (radio) {
+            radio.disabled = true;
+        });
+    }
+
+    function actualizarOpciones(selectId, opciones) {
+        var container = document.getElementById(selectId + '-container');
+        container.innerHTML = '';
+
+        var tituloColumna = document.createElement('p');
+        tituloColumna.appendChild(document.createTextNode('Seleccione ' + selectId));
+        tituloColumna.classList.add('m-0', 'fw-bold');
+        container.appendChild(tituloColumna);
+
+        opciones.forEach(function (opcion, index) {
+            var radioContainer = document.createElement('div'); // Contenedor para radiobutton y label
+
+            var radio = document.createElement('input');
+            radio.type = 'radio';
+            radio.name = selectId + '-radio';
+            radio.value = opcion.posicion;
+
+            // Deshabilitar la última ciudad en origen y la primera ciudad en destino
+            if ((selectId === 'origen' && index === opciones.length - 1) ||
+                (selectId === 'destino' && index === 0)) {
+                radio.disabled = true;
+            }
+
+            var label = document.createElement('label');
+            label.appendChild(document.createTextNode(opcion.nombre));
+
+            radioContainer.appendChild(label);
+            radioContainer.appendChild(radio);
+            radioContainer.classList.add('d-flex', 'align-items-center', 'justify-content-between');
+            container.appendChild(radioContainer);
+
+            // Agregar evento change para habilitar destinos según el origen seleccionado
+            if (selectId === 'origen') {
+                radio.addEventListener('change', function () {
+                    deshabilitarDestinos();
+                    habilitarDestinos(index + 1); // Habilitar destinos a partir de este índice
+                    verificarSeleccion(); // Verificar si ambos radios están seleccionados
+                });
+            } else if (selectId === 'destino') {
+                radio.addEventListener('change', function () {
+                    verificarSeleccion(); // Verificar si ambos radios están seleccionados
+                });
+            }
+        });
+    }
+
+    function habilitarDestinos(desdeIndice) {
+        var radiosDestino = destinos.querySelectorAll('input[type="radio"]');
+        radiosDestino.forEach(function (radio, index) {
+            if (index >= desdeIndice) {
+                radio.disabled = false;
+            }
+        });
+    }
+
+    function verificarSeleccion() {
+        var radioOrigenSeleccionado = origenes.querySelector('input[type="radio"]:checked');
+        var radioDestinoSeleccionado = destinos.querySelector('input[type="radio"]:checked');
+
+        // Verificar si ambos radios de origen y destino están seleccionados
+        if (radioOrigenSeleccionado && radioDestinoSeleccionado) {
+            agregarBoton(); // Agregar el botón al formulario
+        }
+    }
+
+    function agregarBoton() {
+        var boton = document.createElement('button');
+        boton.type = 'button';
+        boton.textContent = 'Enviar';
+        boton.addEventListener('click', function () {
+            // Lógica para el botón (puedes agregar la funcionalidad que desees)
+            alert('Botón presionado');
+        });
+
+        form.appendChild(boton);
+    }
 });
-
-function actualizarOpciones(selectId, opciones) {
-    var container = document.getElementById(selectId + '-container');
-    container.innerHTML = '';
-
-    var tituloColumna = document.createElement('p');
-    tituloColumna.appendChild(document.createTextNode('Seleccione ' + selectId));
-    tituloColumna.classList.add('m-0', 'fw-bold')
-    container.appendChild(tituloColumna);
-    
-    opciones.forEach(function (opcion) {
-        var checkboxContainer = document.createElement('div'); // Contenedor para checkbox y label
-
-        var checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.name = selectId + '-checkbox';  
-        checkbox.value = opcion.posicion;
-
-        var label = document.createElement('label');
-        label.appendChild(document.createTextNode(opcion.nombre));
-
-        checkboxContainer.appendChild(label);
-        checkboxContainer.appendChild(checkbox);
-        checkboxContainer.classList.add('d-flex', 'align-items-center', 'justify-content-between');
-        container.appendChild(checkboxContainer);
-    });
-    
-}
