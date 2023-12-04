41859da0683815181645bfbc57cc9a4deb6f2914
Alejo <alejoczombos@gmail.com>
2023-12-04 19:04:42 -0300

    host

--- ViajaPlus/settings.py
+++ ViajaPlus/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-7sf_zqwl*mhlkf-&&v3df1nc@&*ti6yrn79@89si!^i1vez--^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
