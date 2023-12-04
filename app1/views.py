765d3d6296888585cceaa09e1912719ac0b3867f
Alejo <alejoczombos@gmail.com>
2023-12-04 00:18:28 -0300

    Control de la disponibilidad para crear la reserva

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.db import connection
 
 from .models import *
@@ -70,30 +70,36 @@ def obtener_reservas(request, dni):
                  } 
                 for idPasaje, costo, servicio, DNI, pagado, origen, destino in reservas]
     
-    return {'reservas': reservas}
+    return JsonResponse({'reservas': reservas})
 
 def consultar_disponibilidad(servicio_id):
-    with connection.cursor() as cursor:
-        cursor.execute('''
-            SELECT idPasaje, costo, servicio, DNI, pagado, origen, destino
-            FROM pasaje
-            WHERE DNI = %s
-        ''', [servicio_id])
+    servicio = Servicio.objects.get(numero_servicio=servicio_id)
+    if servicio.disponibilidad > 0:
+        return True
+    else:
+        return False
+
+
 
 def crear_pasaje(request):
     servicio_id = request.POST['servicio']
     servicio_request = get_object_or_404(Servicio, numero_servicio=servicio_id)
 
-    pasaje = Pasaje(
-        servicio=servicio_request,
-        DNI=request.POST['DNI'],
-        origen=request.POST['origen'],
-        destino=request.POST['destino'],
-        costo = 200
-    )
+    disponibilidad = consultar_disponibilidad(servicio_id)
 
-    pasaje.save()
-    return redirect('home')
+    if disponibilidad:
+        pasaje = Pasaje(
+            servicio=servicio_request,
+            DNI=request.POST['DNI'],
+            origen=request.POST['origen'],
+            destino=request.POST['destino'],
+            costo = 200
+        )
+
+        pasaje.save()
+        return redirect('home')
+    else:
+        return HttpResponse("No hay disponibilidad para reservar un pasaje.", status=400)
 
 def prueba(request):
     return render(request, 'prueba.html')
